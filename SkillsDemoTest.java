5b372cccd1a2e61af6daad5e8881eca931790cf9
ctanma <ctma@ucsd.edu>
2022-05-01 16:44:30 -0700

    Update SkillsDemoTest.java

--- SkillsDemoTest.java
+++ SkillsDemoTest.java
@@ -2,5 +2,8 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class SkillsDemoTest {
-    
+    @Test
+    public void firstLetterTest(){
+        assertEquals("d",SkillsDemo.firstLetter("dog")); //expect this to fail bc != "d"
+    }
 }
